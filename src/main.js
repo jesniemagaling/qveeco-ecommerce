a5fbac3e1c5addfc93164b2fa4865203b7e37a77
Jesnie <jesniemagaling05@gmail.com>
2025-07-19 23:48:18 +0800

    creating hero section and make the navbar responsive

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const menuTab = document.querySelector('.menu-tab');
+const closeTab = document.querySelector('.close-tab');
+const navTab = document.querySelector('.nav-links');
+
+menuTab.addEventListener('click', () => {
+  menuTab.classList.add('hidden');
+  closeTab.classList.remove('hidden');
+
+  navTab.classList.remove('hidden');
+});
+
+closeTab.addEventListener('click', () => {
+  closeTab.classList.add('hidden');
+  menuTab.classList.remove('hidden');
+
+  navTab.classList.add('hidden');
+});
