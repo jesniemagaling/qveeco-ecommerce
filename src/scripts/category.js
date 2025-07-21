11d99b6037b61cec9b2b50d9784a93f189c3498e
Jesnie <jesniemagaling05@gmail.com>
2025-07-22 00:00:57 +0800

    fix: vite config

--- src/scripts/category.js
+++ src/scripts/category.js
@@ -1,5 +1,54 @@
 import { products } from './utils/product';
 
+const filterBtn = document.querySelector('.js-filter');
+const filterHeading = document.querySelector('.js-filter-heading');
+const filterCollapse = document.querySelector('.js-collapse');
+const priceBtn = document.querySelector('.js-price');
+const priceCollapse = document.querySelector('.js-price-collapse');
+const colorBtn = document.querySelector('.js-color');
+const colorCollapse = document.querySelector('.js-color-collapse');
+const sizeBtn = document.querySelector('.js-size');
+const sizeCollapse = document.querySelector('.js-size-collapse');
+const styleBtn = document.querySelector('.js-style');
+const styleCollapse = document.querySelector('.js-style-collapse');
+const priceDropdown = document.getElementById('priceDropdownIcon');
+const colorDropdown = document.getElementById('colorDropdownIcon');
+const sizeDropdown = document.getElementById('sizeDropdownIcon');
+const styleDropdown = document.getElementById('styleDropdownIcon');
+const filterIcon = document.getElementById('filterIcon');
+const closeIcon = document.getElementById('closeIcon');
+const filterAside = document.getElementById('filterAside');
+const filterOverlay = document.getElementById('filterOverlay');
+
+filterBtn.addEventListener('click', () => {
+  if (window.innerWidth < 768) {
+    filterCollapse.classList.toggle('max-h-0');
+    filterCollapse.classList.toggle('max-h-220');
+    filterHeading.classList.toggle('hidden');
+    filterIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+    filterAside.classList.toggle('bg-white');
+    filterBtn.classList.toggle('w-fit');
+    filterBtn.classList.toggle('w-full');
+    filterOverlay.classList.toggle('hidden');
+    filterOverlay.classList.toggle('z-0');
+    filterOverlay.classList.toggle('z-10');
+  }
+});
+
+function setupToggle(button, collapse, icon) {
+  button.addEventListener('click', () => {
+    collapse.classList.toggle('max-h-0');
+    collapse.classList.toggle('max-h-96');
+    icon.classList.toggle('rotate-180');
+  });
+}
+
+setupToggle(priceBtn, priceCollapse, priceDropdown);
+setupToggle(colorBtn, colorCollapse, colorDropdown);
+setupToggle(sizeBtn, sizeCollapse, sizeDropdown);
+setupToggle(styleBtn, styleCollapse, styleDropdown);
+
 // all products
 let categoryHTML = '';
 
--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@ export default defineConfig({
   plugins: [tailwindcss()],
 
   server: {
-    open: '/pages/product.html', // Automatically open this page in the browser
+    open: '/pages/category.html', // Automatically open this page in the browser
   },
 
   build: {
     rollupOptions: {
       input: {
         homepage: resolve(__dirname, 'pages/homepage.html'),
-        product: resolve(__dirname, 'pages/product.html'),
+        product: resolve(__dirname, 'pages/category.html'),
         productDetails: resolve(__dirname, 'pages/product-details.html'),
         cart: resolve(__dirname, 'pages/cart.html'),
         checkout: resolve(__dirname, 'pages/checkout.html'),
